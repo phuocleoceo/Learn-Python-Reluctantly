99385a2fecc657aa609cc5fbf56aa8134cbd34d8
phuocleoceo <ht10082001@gmail.com>
2021-10-01 10:31:05 +0700

    fix signal

--- Signal Processing/Energy And Power Of Signal/MyCodeMinimized.py
+++ Signal Processing/Energy And Power Of Signal/MyCodeMinimized.py
@@ -2,11 +2,12 @@ from os.path import dirname, join, abspath
 from scipy.io.wavfile import read
 import numpy as np
 
-file_path = join(dirname(abspath(__file__)), "voice.wav")
+file_path = join(dirname(abspath(__file__)), "studio_male.wav")
 
 frequency, signal = read(file_path)
+signal = np.array([x/32767 for x in signal])
+
 print(frequency, ",", signal)
-signal = np.array(signal, dtype=np.int64)
 
 E = 0
 for x in signal:
@@ -16,3 +17,5 @@ print(">> Energy : ", E)
 L = len(signal)
 P = E/L
 print(">> Power : ", P)
+
+# https://dsp.stackexchange.com/questions/44571/why-is-there-an-amplitude-difference-in-matlab-and-python
